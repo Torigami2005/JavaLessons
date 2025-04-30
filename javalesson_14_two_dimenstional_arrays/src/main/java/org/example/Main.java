2761b9b9d60ac9d616aa660bd7e873f41c555cfa
Torigami <torigamimain@gmail.com>
2025-04-30 19:56:26 +0800

    two-multidimensional arrays on java

--- javalesson_14_two_dimenstional_arrays/src/main/java/org/example/Main.java
+++ javalesson_14_two_dimenstional_arrays/src/main/java/org/example/Main.java
@@ -4,7 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+
+
+        int [][] arr = {
+                {9, 8, 7, 1},
+                {2, 1, 5, 2},
+                {5, 6, 3, 4}
+        };
+        int least = arr [0][0];
+
+        for (int i = 0; i< arr.length; i ++) {
+            for (int j = 0; j < arr[i].length; j ++) {
+                least = least > arr[i][j] ? arr [i] [j] : least;
+            }
+            System.out.println("");
+        }
+        System.out.println("Least: " + least);
+
+       /* Scanner scan = new Scanner(System.in);
         int arr1[][] = new int[4][3];
 
         for(int i = 0; i < arr1[i].length; i++){
@@ -18,7 +35,7 @@ public class Main {
                 System.out.print(arr1[j][i]+" ");
             }
             System.out.println("");
-        }
+        }*/
 
    /*     int [] [] arr = new int[3][]; // [rows] [columns]
         arr[0] = new int [5];
